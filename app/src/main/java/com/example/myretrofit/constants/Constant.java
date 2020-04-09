44e7db1728a04f77ba07d1c873ae01c0d885eb4d
SKY <akashingulkar245@gmail.com>
2020-04-09 13:22:33 +0530

    retrofit

---
+++ app/src/main/java/com/example/myretrofit/constants/Constant.java
@@ -0,0 +1,9 @@
+package com.example.myretrofit.constants;
+
+public class Constant {
+    public static class baseUrl{
+
+        public final static String BASE_URL = "https://akashingulkar245.000webhostapp.com/user_retro_testing/";
+
+    }
+}
